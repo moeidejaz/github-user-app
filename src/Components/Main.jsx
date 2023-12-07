c0e2232eff5c71110f777bc949ad7f30aaebca67
Abdul Moeid <abdulmoeid173@gmail.com>
2023-12-07 23:33:33 +0500

    changes - completed UI - Code cleaning in process

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,74 @@
-import React from 'react'
-import styles from "../styles/main.module.css"
-import SearchBar from './SearchBar'
+import React from "react";
+import styles from "../styles/main.module.css";
+import SearchBar from "./SearchBar";
+
+import profile from "../assets/profile-pic.png";
 
 const Main = () => {
   return (
     <main>
-      <SearchBar/>
-    </main>
-  )
-}
+      <SearchBar />
 
-export default Main
+      <section className={styles.user_details}>
+        {/* ROW ONE */}
+          <img
+            src="https://avatars.githubusercontent.com/u/64331666?v=4"
+            alt="profile photo"
+          />
+          <div>
+            <h1 className={styles.name}>Abdul Moeid</h1>
+            <a href="#" className={styles.username} target="_blank">@moeidejaz</a>
+          </div>
+          <p className={styles.joinDate}>Joined 26 Apr 2020</p>
+
+        {/* ROW TWO */}
+        <div className={styles.user_main}>
+          <p>
+            Self-taught React JS developer, constantly exploring, learning, and
+            turning ideas into reality through code. 🚀
+          </p>
+        </div>
+
+        {/* ROW THREE */}
+        <div className={styles.user_stats}>
+          <div className={styles.repos}>
+            <h5>Repos</h5>
+            <h2>33</h2>
+          </div>
+          <div className={styles.followers}>
+            <h5>Followers</h5>
+            <h2>2</h2>
+          </div>
+          <div className={styles.following}>
+            <h5>Following</h5>
+            <h2 className={styles.l}>1</h2>
+          </div>
+        </div>
+
+        <div className={styles.user_links}>
+          <div className={styles.link}>
+            <img src="/src/assets/location.svg" alt="" />
+            <p>Earth</p>
+          </div>
+
+          <div className={styles.link}>
+            <img src="/src/assets/twitter.svg" alt="" />
+            <p>moeidejaz</p>
+          </div>
+
+          <div className={styles.link}>
+            <img src="/src/assets/link.svg" alt="" />
+            <p>https://moeidejaz.ne...</p>
+          </div>
+
+          <div className={styles.link}>
+            <img src="/src/assets/org.svg" alt="" />
+            <p>Not Available</p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
--- src/utils/ThemeSwitcher.jsx
+++ src/utils/ThemeSwitcher.jsx
@@ -1,10 +1,12 @@
 const lightTheme = [
-  "--body-color: #F6F8FF",
-  "--main-color: #FFFFFF",
+  "--body-bg-color: #F6F8FF",
+  "--main-bg-color: #FFFFFF",
   "--heading-color: #222731",
   "--mode-color: #697C9A;",
+  "--username-color: #0079ff",
   "--stats-bg-color: #F6F8FF",
-  "--text-color: #FFFFFF",
+  "--text-color: #4b6a9b",
+  "--grey-text-color: #2b3442",
   "--btn-bg-color: #0079ff",
   "--btn-hover-color: #60ABFF",
   "--placeholder-color: #222731",
@@ -13,12 +15,14 @@ const lightTheme = [
 ];
 
 const darkTheme = [
-  "--body-color: #141D2F",
-  "--main-color: #1E2A47",
+  "--body-bg-color: #141D2F",
+  "--main-bg-color: #1E2A47",
   "--heading-color: #FFFFFF",
   "--mode-color: #FFFFFF;",
+  "--username-color: #0079ff",
   "--stats-bg-color: #141D2F",
   "--text-color: #FFFFFF",
+  "--grey-text-color: #FFFFFF",
   "--btn-bg-color: #0079ff",
   "--btn-hover-color: #60ABFF",
   "--placeholder-color: #FFFFFF",
